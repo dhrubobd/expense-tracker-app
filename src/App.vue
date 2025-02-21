718e61aae4049e5035c350c0c856086c8383e681
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-02-21 13:49:16 +0600

    Transaction list show is done with type class

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <script setup>
+import { ref,computed,reactive } from 'vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+const transactions = reactive([
+  { title: 'Salary', amount: 5000, type: 'income' },
+  { title: 'Rent', amount: 1000, type: 'expense' },
+  { title: 'Food', amount: 200, type: 'expense' },
+  { title: 'Transport', amount: 50, type: 'expense' },
+]);
 
 </script>
 
 <template>
   <div>
-
+    <h1>Simple Expense Tracker Apps</h1>
   </div>
-
+  <div>
+    <TransactionList :transactions="transactions"/>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps(['transactions']);
+const amountClass = (transaction) => {
+    return transaction.type === 'income' ? 'text-success' : 'text-danger';
+}
+const typeClass = (transaction) => {
+    return transaction.type === 'income' ? 'text-success' : 'text-danger';
+}
+</script>
+
+<template>
+    <div>
+        <table class="table table-bordered mt-3" v-if="transactions.length">
+        <thead>
+            <tr>
+            <th>Title</th>
+            <th>Amount</th>
+            <th>Type</th>
+            <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(transaction, index) in transactions" :key="index">
+            <td>{{ transaction.title }}</td>
+            <td :class="amountClass(transaction)">${{ transaction.amount }}</td>
+            <td :class="typeClass(transaction)">{{ transaction.type }}</td>
+            <td><button @click="$emit('delete-transaction', index)" class="btn btn-danger">Delete</button></td>
+            </tr>
+        </tbody>
+        </table>
+        <p v-else  class="text-center">No transactions recorded yet.</p>
+     </div>
+</template>
+
+<style scoped>
+
+</style>
